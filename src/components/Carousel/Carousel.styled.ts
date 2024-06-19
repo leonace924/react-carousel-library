95b798c840dd46012e54979d78923e509f88f7d2
leonace924 <meobius123@gmail.com>
2024-06-19 11:47:39 -0400

    fix: finalize with styled components

--- src/components/Carousel/Carousel.styled.ts
+++ src/components/Carousel/Carousel.styled.ts
@@ -1,10 +1,34 @@
 import styled from '@emotion/styled'
+import { animated } from '@react-spring/web'
 
 export const CarouselContainer = styled.div`
   position: relative;
   overflow: hidden;
   width: 100%;
 `
+
+export const CarouselWrapper = styled.div`
+  display: flex;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`
+
+export const CarouselTrack = styled(animated.div)`
+  display: flex;
+  top: 0;
+  left: 0;
+  position: relative;
+  width: 100%;
+  touch-action: none;
+`
+
+export const CarouselItem = styled.div<{itemsPerSlide: number}>`
+  display: flex;
+  position: relative;
+  flex: 1 0 calc(100% / ${({ itemsPerSlide }) => itemsPerSlide});
+`;
 
 export const CarouselButton = styled.button`
   position: absolute;
--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,28 +1,41 @@
-import React, { useRef, useCallback, useEffect, ReactNode } from "react";
+/** @jsxImportSource @emotion/react */
+import React, {
+  useRef,
+  useCallback,
+  useEffect,
+  ReactNode,
+} from 'react'
 import {
   useSpring,
   config,
-  animated,
   AnimationResult,
-} from "@react-spring/web";
-import { useDrag } from "@use-gesture/react";
-import { useCustomEventsModule } from "../modules";
-import { SlideToItemFnProps, SlideActionType } from "./types";
-import { useMount } from "../utils";
-import { CarouselContainer, Div, NextButton, PrevButton } from "./Carousel.styled";
+} from '@react-spring/web'
+import { useDrag } from '@use-gesture/react'
+import { useCustomEventsModule } from '../modules'
+import { SlideToItemFnProps, SlideActionType } from './types'
+import { useMount } from '../utils'
+import {
+  CarouselContainer,
+  CarouselItem,
+  CarouselTrack,
+  CarouselWrapper,
+  Div,
+  NextButton,
+  PrevButton,
+} from './Carousel.styled'
 
 export interface CarouselProps {
-  children: ReactNode;
-  isInfinite?: boolean;
-  hasNavigation?: boolean;
-  draggingSlideTreshold?: number;
-  springConfig?: typeof config;
-  shouldResizeOnWindowResize?: boolean;
-  carouselSlideAxis?: "x" | "y";
-  itemsPerSlide?: number;
-  initialActiveItem?: number;
-  initialStartingPosition?: "start" | "center" | "end";
-  disableGestures?: boolean;
+  children: ReactNode
+  isInfinite?: boolean
+  hasNavigation?: boolean
+  draggingSlideTreshold?: number
+  springConfig?: typeof config
+  shouldResizeOnWindowResize?: boolean
+  carouselSlideAxis?: 'x' | 'y'
+  itemsPerSlide?: number
+  initialActiveItem?: number
+  initialStartingPosition?: 'start' | 'center' | 'end'
+  disableGestures?: boolean
 }
 
 const Carousel: React.FC<CarouselProps> = ({
@@ -32,85 +45,88 @@ const Carousel: React.FC<CarouselProps> = ({
   draggingSlideTreshold = 100,
   springConfig = config.default,
   shouldResizeOnWindowResize = true,
-  carouselSlideAxis = "x",
+  carouselSlideAxis = 'x',
   itemsPerSlide = 1,
   initialActiveItem = 0,
-  initialStartingPosition = "start",
+  initialStartingPosition = 'start',
   disableGestures = false,
 }) => {
-  const lastItemReached = useRef(false);
-  const slideActionType = useRef<SlideActionType>("next");
-  const activeItem = useRef(initialActiveItem);
-  const mainCarouselWrapperRef = useRef<HTMLDivElement | null>(null);
-  const carouselTrackWrapperRef = useRef<HTMLDivElement | null>(null);
-  const isDragging = useRef(false);
-  const isAnimating = useRef(false);
-  const windowIsHidden = useRef(false);
-  const currentWindowWidth = useRef(0);
+  const lastItemReached = useRef(false)
+  const slideActionType = useRef<SlideActionType>('next')
+  const activeItem = useRef(initialActiveItem)
+  const mainCarouselWrapperRef = useRef<HTMLDivElement | null>(null)
+  const carouselTrackWrapperRef = useRef<HTMLDivElement | null>(null)
+  const isDragging = useRef(false)
+  const isAnimating = useRef(false)
+  const windowIsHidden = useRef(false)
+  const currentWindowWidth = useRef(0)
 
-  const childArray = React.Children.toArray(children);
+  const childArray = React.Children.toArray(children)
   const internalChildren = isInfinite
     ? [...childArray, ...childArray, ...childArray]
-    : childArray;
+    : childArray
 
-  const { emitObservable } = useCustomEventsModule();
+  const { emitObservable } = useCustomEventsModule()
 
   const [carouselStyles, setCarouselStyles] = useSpring(() => ({
     y: 0,
     x: 0,
     config: springConfig,
-  }));
+  }))
 
   const getSlideValue = useCallback(() => {
     if (!carouselTrackWrapperRef.current) {
-      return 0;
+      return 0
     }
     const carouselItem = carouselTrackWrapperRef.current
-      .firstChild as HTMLElement;
+      .firstChild as HTMLElement
 
     if (!carouselItem) {
-      throw Error("No carousel items available!");
+      throw Error('No carousel items available!')
     }
 
-    return carouselSlideAxis === "x"
+    return carouselSlideAxis === 'x'
       ? carouselItem.getBoundingClientRect().width
-      : carouselItem.getBoundingClientRect().height;
-  }, [carouselSlideAxis]);
+      : carouselItem.getBoundingClientRect().height
+  }, [carouselSlideAxis])
 
   const adjustCarouselWrapperPosition = useCallback(
     (ref: HTMLDivElement) => {
-      const positionProperty = carouselSlideAxis === "x" ? "left" : "top";
-      const getDefaultPositionValue = () => getSlideValue() * childArray.length;
+      const positionProperty =
+        carouselSlideAxis === 'x' ? 'left' : 'top'
+      const getDefaultPositionValue = () =>
+        getSlideValue() * childArray.length
       const setPosition = (v: number) => {
-        ref.style.top = "0px";
-        ref.style.left = "0px";
-        ref.style[positionProperty] = `-${v}px`;
-      };
-      const setStartPosition = () => setPosition(getDefaultPositionValue());
+        ref.style.top = '0px'
+        ref.style.left = '0px'
+        ref.style[positionProperty] = `-${v}px`
+      }
+      const setStartPosition = () =>
+        setPosition(getDefaultPositionValue())
       const setCenterPosition = () =>
         setPosition(
           getDefaultPositionValue() -
-            getSlideValue() * Math.round((itemsPerSlide - 1) / 2)
-        );
+            getSlideValue() * Math.round((itemsPerSlide - 1) / 2),
+        )
       const setEndPosition = () =>
         setPosition(
           getDefaultPositionValue() -
-            getSlideValue() * Math.round(itemsPerSlide - 1)
-        );
+            getSlideValue() * Math.round(itemsPerSlide - 1),
+        )
 
       if (itemsPerSlide > 1) {
         switch (initialStartingPosition) {
-          case "center":
-            setCenterPosition();
-            break;
-          case "end":
-            setEndPosition();
-            break;
+          case 'center':
+            setCenterPosition()
+            break
+          case 'end':
+            setEndPosition()
+            break
           default:
-            setStartPosition();
+            setStartPosition()
         }
       } else {
-        setStartPosition();
+        setStartPosition()
       }
     },
     [
@@ -119,27 +135,27 @@ const Carousel: React.FC<CarouselProps> = ({
       initialStartingPosition,
       childArray.length,
       itemsPerSlide,
-    ]
-  );
+    ],
+  )
 
   const handleResize = useCallback(() => {
     if (window.innerWidth === currentWindowWidth.current) {
-      return;
+      return
     }
-    currentWindowWidth.current = window.innerWidth;
+    currentWindowWidth.current = window.innerWidth
 
     setCarouselStyles.start({
       immediate: true,
       x: 0,
       y: 0,
-    });
+    })
     setCarouselStyles.start({
       immediate: true,
       [carouselSlideAxis]: -(getSlideValue() * activeItem.current),
-    });
+    })
 
     if (isInfinite) {
-      adjustCarouselWrapperPosition(carouselTrackWrapperRef.current!);
+      adjustCarouselWrapperPosition(carouselTrackWrapperRef.current!)
     }
   }, [
     adjustCarouselWrapperPosition,
@@ -147,114 +163,127 @@ const Carousel: React.FC<CarouselProps> = ({
     getSlideValue,
     setCarouselStyles,
     isInfinite,
-  ]);
+  ])
 
   useEffect(() => {
     if (shouldResizeOnWindowResize) {
-      window.addEventListener("resize", handleResize);
+      window.addEventListener('resize', handleResize)
       return () => {
-        window.removeEventListener("resize", handleResize);
-      };
+        window.removeEventListener('resize', handleResize)
+      }
     }
-  }, [handleResize, shouldResizeOnWindowResize]);
+  }, [handleResize, shouldResizeOnWindowResize])
 
   useEffect(() => {
     if (carouselTrackWrapperRef.current) {
-      carouselTrackWrapperRef.current.style.top = "0px";
-      carouselTrackWrapperRef.current.style.left = "0px";
+      carouselTrackWrapperRef.current.style.top = '0px'
+      carouselTrackWrapperRef.current.style.left = '0px'
     }
-  }, [carouselSlideAxis]);
+  }, [carouselSlideAxis])
 
   useMount(() => {
-    currentWindowWidth.current = window.innerWidth;
+    currentWindowWidth.current = window.innerWidth
     const handleVisibilityChange = () => {
-      windowIsHidden.current = document.hidden;
-    };
-    document.addEventListener("visibilitychange", handleVisibilityChange);
+      windowIsHidden.current = document.hidden
+    }
+    document.addEventListener(
+      'visibilitychange',
+      handleVisibilityChange,
+    )
     return () =>
-      document.removeEventListener("visibilitychange", handleVisibilityChange);
-  });
+      document.removeEventListener(
+        'visibilitychange',
+        handleVisibilityChange,
+      )
+  })
 
   useMount(() => {
-    if (initialActiveItem > 0 && initialActiveItem <= childArray.length) {
-      slideToItem({ to: initialActiveItem, immediate: true });
-      activeItem.current = initialActiveItem;
+    if (
+      initialActiveItem > 0 &&
+      initialActiveItem <= childArray.length
+    ) {
+      slideToItem({ to: initialActiveItem, immediate: true })
+      activeItem.current = initialActiveItem
     }
-  });
+  })
 
   useEffect(() => {
     if (carouselTrackWrapperRef.current) {
       const items = carouselTrackWrapperRef.current.querySelectorAll(
-        ".use-spring-carousel-item"
-      );
-      const lastItem = items[items.length - 1];
+        '.pegasus-carousel-item',
+      )
+      const lastItem = items[items.length - 1]
       const observer = new IntersectionObserver(
-        (entries) => {
-          const lastItemEntry = entries[0];
-          lastItemReached.current = lastItemEntry.isIntersecting;
+        entries => {
+          const lastItemEntry = entries[0]
+          lastItemReached.current = lastItemEntry.isIntersecting
         },
-        { threshold: 1 }
-      );
-      observer.observe(lastItem);
-      return () => observer.unobserve(lastItem);
+        { threshold: 1 },
+      )
+      observer.observe(lastItem)
+      return () => observer.unobserve(lastItem)
     }
-  }, [itemsPerSlide]);
+  }, [itemsPerSlide])
 
   const bindDrag = useDrag(
-    (props) => {
-      const isDragging = props.dragging;
-      const movement = props.movement[carouselSlideAxis === "x" ? 0 : 1];
+    props => {
+      const isDragging = props.dragging
+      const movement =
+        props.movement[carouselSlideAxis === 'x' ? 0 : 1]
 
-      const currentSlidedValue = -(getSlideValue() * getCurrentActiveItem());
+      const currentSlidedValue = -(
+        getSlideValue() * getCurrentActiveItem()
+      )
 
-      function resetAnimation() {
+      const resetAnimation = () => {
         setCarouselStyles.start({
           [carouselSlideAxis]: currentSlidedValue,
-        });
+        })
       }
 
       if (isDragging) {
         setCarouselStyles.start({
           [carouselSlideAxis]: currentSlidedValue + movement,
-        });
-        // setIsDragging(true);
-        emitObservable({
-          eventName: "onDrag",
-          ...props,
-        });
+        })
 
-        const prevItemTreshold = movement > draggingSlideTreshold;
-        const nextItemTreshold = movement < -draggingSlideTreshold;
+        emitObservable({
+          eventName: 'onDrag',
+          ...props,
+        })
+
+        const prevItemTreshold = movement > draggingSlideTreshold
+        const nextItemTreshold = movement < -draggingSlideTreshold
 
         if (nextItemTreshold) {
           if (!isInfinite && getIsLastItem()) {
-            resetAnimation();
+            resetAnimation()
           } else {
-            slideToNextItem();
+            slideToNextItem()
           }
-          props.cancel();
+          props.cancel()
         } else if (prevItemTreshold) {
           if (!isInfinite && getIsFirstItem()) {
-            resetAnimation();
+            resetAnimation()
           } else {
-            slideToPrevItem();
+            slideToPrevItem()
           }
-          props.cancel();
+          props.cancel()
         }
       }
 
       if (props.last) {
-        resetAnimation();
+        resetAnimation()
       }
     },
     {
       enabled: !disableGestures,
-    }
-  );
+    },
+  )
 
-  const getCurrentActiveItem = () => activeItem.current;
-  const getIsFirstItem = () => getCurrentActiveItem() === 0;
-  const getIsLastItem = () => getCurrentActiveItem() === childArray.length - 1;
+  const getCurrentActiveItem = () => activeItem.current
+  const getIsFirstItem = () => getCurrentActiveItem() === 0
+  const getIsLastItem = () =>
+    getCurrentActiveItem() === childArray.length - 1
 
   const slideToItem = ({
     from,
@@ -263,17 +292,17 @@ const Carousel: React.FC<CarouselProps> = ({
     onRest = () => {},
   }: SlideToItemFnProps) => {
     if (!immediate) {
-      activeItem.current = to;
-      isAnimating.current = true;
+      activeItem.current = to
+      isAnimating.current = true
       emitObservable({
-        eventName: "onSlideStartChange",
+        eventName: 'onSlideStartChange',
         nextItem: to,
         slideActionType: slideActionType.current,
-      });
+      })
     }
 
     const getFromValue = () =>
-      from ? { from: { [carouselSlideAxis]: from } } : {};
+      from ? { from: { [carouselSlideAxis]: from } } : {}
 
     setCarouselStyles.start({
       ...getFromValue(),
@@ -281,70 +310,76 @@ const Carousel: React.FC<CarouselProps> = ({
       immediate,
       onRest: (val: AnimationResult) => {
         if (val.finished) {
-          isDragging.current = false;
-          isAnimating.current = false;
-          onRest();
+          isDragging.current = false
+          isAnimating.current = false
+          onRest()
 
           if (!immediate) {
             emitObservable({
-              eventName: "onSlideChange",
+              eventName: 'onSlideChange',
               currentItem: getCurrentActiveItem(),
               slideActionType: slideActionType.current,
-            });
+            })
           }
         }
       },
-    });
-  };
+    })
+  }
 
   const slideToPrevItem = () => {
-    if ((!isInfinite && getCurrentActiveItem() === 0) || windowIsHidden.current)
-      return;
+    if (
+      (!isInfinite && getCurrentActiveItem() === 0) ||
+      windowIsHidden.current
+    )
+      return
 
-    slideActionType.current = "prev";
+    slideActionType.current = 'prev'
     if (getIsFirstItem()) {
       slideToItem({
         from: -(
-          Math.abs(getWrapperFromValue(carouselTrackWrapperRef.current!)) +
+          Math.abs(
+            getWrapperFromValue(carouselTrackWrapperRef.current!),
+          ) +
           getSlideValue() * childArray.length
         ),
         to: childArray.length - 1,
-      });
+      })
     } else {
-      slideToItem({ to: getCurrentActiveItem() - 1 });
+      slideToItem({ to: getCurrentActiveItem() - 1 })
     }
-  };
+  }
 
   const slideToNextItem = () => {
     if (
-      (!isInfinite && getCurrentActiveItem() === internalChildren.length - 1) ||
+      (!isInfinite &&
+        getCurrentActiveItem() === internalChildren.length - 1) ||
       windowIsHidden.current ||
       lastItemReached.current
     )
-      return;
+      return
 
-    slideActionType.current = "next";
+    slideActionType.current = 'next'
     if (getIsLastItem()) {
       slideToItem({
         from:
           getWrapperFromValue(carouselTrackWrapperRef.current!) +
           getSlideValue() * childArray.length,
         to: 0,
-      });
+      })
     } else {
-      slideToItem({ to: getCurrentActiveItem() + 1 });
+      slideToItem({ to: getCurrentActiveItem() + 1 })
     }
-  };
+  }
 
   const getWrapperFromValue = (element: HTMLDivElement) => {
-    if (element.style.transform === "none") return 0;
-    const values = element.style.transform.split(/\w+\(|\);?/);
+    if (element.style.transform === 'none') return 0
+    const values = element.style.transform.split(/\w+\(|\);?/)
     return Number(
       values[1]
         .split(/,\s?/g)
-        [carouselSlideAxis === "x" ? 0 : 1].replace("px", "")
-    );
-  };
+        [carouselSlideAxis === 'x' ? 0 : 1].replace('px', ''),
+    )
+  }
 
   return (
     <CarouselContainer>
@@ -355,55 +390,36 @@ const Carousel: React.FC<CarouselProps> = ({
         </Div>
       )}
 
-      <div
-        ref={mainCarouselWrapperRef}
-        style={{
-          display: "flex",
-          position: "relative",
-          width: "100%",
-          height: "100%",
-          overflow: "hidden",
-        }}
-      >
-        <animated.div
+      <CarouselWrapper ref={mainCarouselWrapperRef}>
+        <CarouselTrack
           {...bindDrag()}
           style={{
-            display: "flex",
-            flexDirection: carouselSlideAxis === "x" ? "row" : "column",
-            top: 0,
-            left: 0,
-            position: "relative",
-            width: "100%",
+            flexDirection:
+              carouselSlideAxis === 'x' ? 'row' : 'column',
             ...carouselStyles,
-            touchAction: "none",
           }}
-          ref={(ref) => {
+          ref={ref => {
             if (ref) {
-              carouselTrackWrapperRef.current = ref;
+              carouselTrackWrapperRef.current = ref
               if (isInfinite) {
-                adjustCarouselWrapperPosition(ref);
+                adjustCarouselWrapperPosition(ref)
               }
             }
           }}
         >
           {internalChildren.map((child, index) => (
-            <div
+            <CarouselItem
               key={index}
-              className="use-spring-carousel-item"
-              data-testid="use-spring-carousel-item-wrapper"
-              style={{
-                display: "flex",
-                position: "relative",
-                flex: `1 0 calc(100% / ${itemsPerSlide})`,
-              }}
+              className="pegasus-carousel-item"
+              itemsPerSlide={itemsPerSlide}
             >
               {child}
-            </div>
+            </CarouselItem>
           ))}
-        </animated.div>
-      </div>
+        </CarouselTrack>
+      </CarouselWrapper>
     </CarouselContainer>
-  );
-};
+  )
+}
 
-export default Carousel;
+export default Carousel
